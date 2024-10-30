19cd799e50392397f3601f1a04b702f12828b2fc
utkarshsharma <uvutkarshvs@gmail.com>
2024-10-30 20:52:20 +0100

    Completed inclding KAT's model in API and deplyed the first working API. Please NOTE -THIS VERSION HAS CHANGES MADE BY ME IN KAT'S NOTEBOOK. WE MIGHT NEED TO TREAT IT CAREFULLY DURING MASTER MERGE

--- package_folder/api_file.py
+++ package_folder/api_file.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
 import pickle
 
 from package_folder.predict_model import my_prediction_function
@@ -11,11 +11,11 @@ app = FastAPI()
 def root():
     return {'greeting':"hello from utkarsh"}
 
-# # Prediction endpoint
-# @app.get("/predict")
-# def predict():
-#     # Use the function in our package to run the prediction
-#     prediction = my_prediction_function()
+# Prediction endpoint
+@app.get("/predict")
+def predict(bedrooms, bathrooms, living_area, surface_of_the_plot, postal):
+    # Use the function in our package to run the prediction
+    prediction = my_prediction_function(bedrooms, bathrooms, living_area, surface_of_the_plot, postal)
 
-#     # Return prediction
-#     return {"prediction": int(prediction[0])}
+    # Return prediction
+    return {"prediction": int(prediction)}
--- package_folder/predict_model.py
+++ package_folder/predict_model.py
@@ -1,16 +1,26 @@
-# import os
-# import pickle
+import os
+import pickle
 
-# ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
+ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
 
-def my_prediction_function(name):
-    
-    # # Load the model from the pickle file
-    # model_path = os.path.join(ROOT_PATH, 'models', 'best_model.pkl')
-    # with open(model_path, 'rb') as file:
-    #     model = pickle.load(file)
+def my_prediction_function(bedrooms, bathrooms, living_area, surface_of_the_plot, postal):
+    """Prediction function using a pretrained model loaded from disk
 
-    # # Use the model to predict the given inputs
-    # prediction = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
-    print(f"Hello {name}")
+    Arguments:
+    - bedrooms
+    - bathrooms
+    - living_area
+    - surface_of_the_plot
+    - postal
+    """
+    # Load the model from the pickle file
+    model_path = os.path.join(ROOT_PATH, 'models', 'best_model.pkl')
+    with open(model_path, 'rb') as file:
+        model = pickle.load(file)
 
+    # Use the model to predict the given inputs
+    prediction = model.predict([[float(bedrooms), float(bathrooms), float(living_area), float(surface_of_the_plot), float(postal)]])
+
+    return prediction[0]
+
+print(my_prediction_function(2,1,70,80,84773.0))
