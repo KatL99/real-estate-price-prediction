4867a42788ffc5d2dc65f0200dc0fd60fdf08655
Alexander Kloeck <alexander.kloeck@escandria.be>
2024-10-30 15:36:28 +0100

    updated frontend

--- frontend/frontend_file.py
+++ frontend/frontend_file.py
@@ -9,24 +9,29 @@ st.title("Real Estate Price Prediction MVP")
 st.write("Real Estate Price Predictor")
 
 
-bedrooms = st.slider('How many bedrooms', min_value=0, max_value=5, value=1, step=1)
-bathrooms = st.slider('How many bathrooms',  min_value=0, max_value=5, value=1, step=1)
-
-living_area = st.number('How much living area?',  min_value=0, max_value=200, value=1, step=5)
-surface_of_the_plot = st.number('Which surface of the plot?',  min_value=0, max_value=500, value=1, step=5)
-
-postal = st.number('What is the postal code? (from 1000 to 9999)',  min_value=1000, max_value=9999, value=1000, step=1)
-
+bedrooms = st.slider('How many bedrooms?', min_value=0, max_value=5, value=1, step=1)
+bathrooms = st.slider('How many bathrooms?',  min_value=0, max_value=5, value=1, step=1)
+living_area = st.number_input('living area in m2?')
+surface_of_the_plot = st.number_input('Surface of the plot in m2?')
+postal = st.number_input('Postal code? (from 1000 to 9999)')
 
 
 url = f"{API_URL}/predict"
 params = {
     'bedrooms': bedrooms,
     'bathrooms': bathrooms,
+    'living_area': living_area,
     'surface_of_the_plot ': surface_of_the_plot,
     'postal': postal,
 }
 
-response = requests.get(url, params=params).json()
+#response = requests.get(url, params=params).json()
+response = 350000
 
-st.write(f"The estimated price is {str(response[' Euro'])}")
+st.markdown("""
+
+    ## Estimated price:
+
+""")
+
+st.write(f"The estimated price is {str(response)} .")
