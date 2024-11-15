52961637b2fe29710478497e10d6700132d3ce7d
utkarshsharma <uvutkarshvs@gmail.com>
2024-11-15 19:38:54 +0100

    updated API, Model.predict

--- package_folder/api_file.py
+++ package_folder/api_file.py
@@ -1,21 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Query
 import pickle
-
 from package_folder.predict_model import my_prediction_function
 
 # FastAPI instance
 app = FastAPI()
 
+  
 # Root endpoint
 @app.get("/")
 def root():
     return {'greeting':"hello from utkarsh"}
 
 # Prediction endpoint
-@app.get("/predict")
-def predict(bedrooms, bathrooms, living_area, surface_of_the_plot, postal):
+@app.get("/predict") #added this part today
+# def predict(
+#      bedrooms: int= Query(...),
+#      building_condition: str= Query(...),
+#      construction_year: int= Query(...),
+#      double_glazing: int= Query(...),
+#      energy_class: str= Query(...),
+#      furnished: int= Query(...),
+#      surface_of_the_plot:int= Query(...),
+#      tenement_building: int= Query(...),
+#      toilets: int= Query(...),
+#      city:str= Query(...),
+#       ):
+def predict(bedrooms, building_condition, construction_year,
+            double_glazing,energy_class, furnished,
+            surface_of_the_plot, tenement_building,
+            toilets, city):
     # Use the function in our package to run the prediction
-    prediction = my_prediction_function(bedrooms, bathrooms, living_area, surface_of_the_plot, postal)
+    prediction = my_prediction_function(bedrooms, building_condition, construction_year,
+                           double_glazing,energy_class, furnished,
+                           surface_of_the_plot, tenement_building,
+                           toilets, city)
 
     # Return prediction
     return {"prediction": int(prediction)}
